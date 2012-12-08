dfcdc8ec1bcfa5ef57a6911082c3332bd676f111
ddeutsch <ddeutsch@jhu.edu>
2012-12-07 23:22:43 -0500

    Added removeIngredient.php

---
+++ pages/removeIngredient.php
@@ -0,0 +1,23 @@
+<?php
+    session_start();
+    $db_host = 'localhost:8888';
+    $db_user = 'cs41512_recman';
+    $db_pass = 'pass';
+    $db_name = 'cs41512_recipe_db';
+
+    $conn = mysql_connect($db_host, $db_user, $db_pass);
+    if (!$conn)
+    {
+	echo "Error connecting to the database";
+	exit();
+    }
+
+    mysql_select_db($db_name, $conn);
+    
+    $ingredient = $_POST['ingredient'];
+
+    $query = "DELETE FROM Cabinet WHERE Username = '".$_SESSION['username']."' AND Ingredient ='".$ingredient."'";
+    //echo $query;
+    mysql_query($query);
+    header('Location: cabinet.php');
+?>
